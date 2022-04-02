be8d8598d0a67e542e35e8f28464491d6a2ee8dc
VoidChacal <92901966+VoidChacal@users.noreply.github.com>
2022-04-02 09:40:21 -0300

    Add files via upload

---
+++ CMD Message Teste.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+@echo.
+@echo    ***********************************************
+@echo    ***                                         ***
+@echo    ***         LOCKDOWN MESSAGE TESTE 	       ***
+@echo    ***                                         ***
+@echo    ***********************************************
+@echo.
+pause
+:LOOP
+%random% %random% %random%
+%random% %random% %random%
+%random% %random% %random% 
+GOTO LOOP
