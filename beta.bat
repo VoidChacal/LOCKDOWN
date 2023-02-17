ad3c79ae6d40858d386bd22489d68c0c80c22457
VoidChacal <92901966+VoidChacal@users.noreply.github.com>
2023-02-17 11:32:38 -0300

    Add files via upload

--- beta.bat
+++ beta.bat
@@ -1,4 +1,12 @@
 @echo off
+echo 1 Apagar arquivos temporarios
+echo 2 Apagar Logs
+echo 3 Distribution
+set /p resposta=
+if %resposta%==1 goto Temporarios
+if %resposta%==2 goto logs
+if %resposta%==3 goto Distribution
+:Temporarios
 RD /S /Q %temp%
 MKDIR %temp%
 takeown /f "%temp%" /r /d y
@@ -8,12 +16,14 @@ MKDIR C:\Windows\Temp
 takeown /f "C:\Windows\Temp" /r /d y
 takeown /f %temp% /r /d y
 pause
+:logs
 REM parte 2
 cd/
 @echo
 del *.log /a /s /q /f
 pause
 REM parte 3
+:Distribution
 net stop wuauserv
 net stop UsoSvc
 rd /s /q C:\Windows\SoftwareDistribution
