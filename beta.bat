b7368a684cb270e268d8aeca05cdaf84e11d87a2
VoidChacal <92901966+VoidChacal@users.noreply.github.com>
2023-02-18 21:42:28 -0300

    Add files via upload

--- beta.bat
+++ beta.bat
@@ -1,7 +1,19 @@
 @echo off
-echo 1 Apagar arquivos temporarios
-echo 2 Apagar Logs
-echo 3 Distribution
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+:menu
+@echo.
+@echo    *************************************************************************
+@echo    ***                                        			         *** Nota: Provavelmente ha um
+@echo    ***         Sistema batchfile de limpeza de computadores	       	 ***      programa melhor que este
+@echo    ***                                                                   ***	  
+@echo    *************************************************************************	 
+@echo.
+call :ColorText 0c "1 Apagar arquivos temporarios"
+call :ColorText 0c "2 Apagar Logs"
+call :ColorText 0c "3 Distribution"
 set /p resposta=
 if %resposta%==1 goto Temporarios
 if %resposta%==2 goto logs
@@ -16,12 +28,16 @@ MKDIR C:\Windows\Temp
 takeown /f "C:\Windows\Temp" /r /d y
 takeown /f %temp% /r /d y
 pause
+cls
+goto menu
 :logs
 REM parte 2
 cd/
 @echo
 del *.log /a /s /q /f
 pause
+cls
+goto menu
 REM parte 3
 :Distribution
 net stop wuauserv
@@ -29,3 +45,10 @@ net stop UsoSvc
 rd /s /q C:\Windows\SoftwareDistribution
 md C:\Windows\SoftwareDistribution
 pause
+cls
+goto menu
+:ColorText
+echo off
+echo %DEL% > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
