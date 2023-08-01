d0c1bbd4c2c3d87ae80a810d41cb8107a8716161
VoidChacal <92901966+VoidChacal@users.noreply.github.com>
2023-08-01 11:39:11 -0300

    Update beta.bat

--- beta.bat
+++ beta.bat
@@ -5,16 +5,34 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
 )
 :menu
 @echo.
-@echo    *************************************************************************   Nota: Provavelmente ha um
-@echo    ***                                        			         ***        programa melhor que este
-@echo    ***         Sistema batchfile de limpeza de computadores	       	 ***      
+@echo    *************************************************************************   
+@echo    ***                                        			         ***        
+@echo    ***         		   Multipurpose tools	       	 		 ***      
 @echo    ***                                                                   ***   Aviso: Executar o arquivo 	  
 @echo    *************************************************************************         no modo administrador 
 @echo.
 call :ColorText 0c "1 Apagar arquivos temporarios"
+call :ColorText 0c "2 Ativacao do Windows"
 set /p resposta=
- if %resposta%==1 ( goto Temporarios
- ) else ( 
+if %resposta%==2( goto Activation )
+if %resposta%==1( goto limpeza )
+:limpeza
+@echo.
+@echo    *************************************************************************   
+@echo    ***                                        			         ***        
+@echo    ***         Sistema batchfile de limpeza de computadores	       	 ***      
+@echo    ***                                                                   ***   
+@echo    *************************************************************************         
+@echo.
+call :ColorText 0c "1 Apagar arquivos temporarios"
+set /p resposta2=
+ if %resposta2%==1 ( goto Temporarios
+ )
+REM " SKILL ISSUE"
+if %resposta2%==2 ( goto Activation
+ ) 
+REM " SKILL ISSUE"
+else ( 
  cls
  echo.
  call :ColorText 02 "   &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& "
@@ -53,6 +71,47 @@ del *.log /a /s /q /f
 pause
 cls
 goto menu
+:Activation
+@echo.
+@echo    *************************************************************************   
+@echo    ***                                        			         ***        
+@echo    ***                        Windows Activation	       	 		***      
+@echo    ***                                                                   ***   
+@echo    *************************************************************************         
+@echo.
+call :ColorText 0c "1 Home/Core"
+call :ColorText 0c "2 Home/Core (Country Specific)"
+call :ColorText 0c "3 Home/Core (Single Language)"
+call :ColorText 0c "4 Home/Core N"
+call :ColorText 0c "5 Professional(Wind 10 pro)"
+call :ColorText 0c "6 Professional N(Wind 10 pro N)"
+call :ColorText 0c "7 Enterprise"
+call :ColorText 0c "8 Enterprise N"
+call :ColorText 0c "9 Education"
+call :ColorText 0c "10 Education N"
+call :ColorText 0c "11 Enterprise 2015 LTSB"
+call :ColorText 0c "12 Enterprise 2015 LTSB N"
+call :ColorText 0c "13 Enterprise 2016 LTSB"
+call :ColorText 0c "14 Enterprise 2016 LTSB N"
+set /p resposta3=
+if %resposta3%==1( %key%= TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 )
+if %resposta3%==2( %key%= PVMJN-6DFY6-9CCP6-7BKTT-D3WVR )
+if %resposta3%==3( %key%= 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH )
+if %resposta3%==4( %key%= 3KHY7-WNT83-DGQKR-F7HPR-844BM )
+if %resposta3%==5( %key%= W269N-WFGWX-YVC9B-4J6C9-T83GX )
+if %resposta3%==6( %key%= MH37W-N47XK-V7XM9-C7227-GCQG9 )
+if %resposta3%==7( %key%= NPPR9-FWDCX-D2C8J-H872K-2YT43 )
+if %resposta3%==8( %key%= DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4 )
+if %resposta3%==9( %key%= NW6C2-QMPVW-D7KKK-3GKT6-VCFB2 )
+if %resposta3%==10( %key%= 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ )
+if %resposta3%==11( %key%= WNMTR-4C88C-JK8YV-HQ7T2-76DF9 )
+if %resposta3%==12( %key%= 2F77B-TNFGY-69QQF-B8YKP-D69TJ )
+if %resposta3%==13( %key%= DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ )
+if %resposta3%==14( %key%= QFFDN-GRT3P-VKWWX-X7T3R-8B639 )
+if %resposta3%==15( %key%= teste )
+cscript slmgr.vbs /ipk %key%
+cscript slmgr.vbs /skms kms.lotro.cc
+cscript slmgr.vbs /ato
 :ColorText
 echo off
 echo %DEL% > "%~2"
